16a0352a0e5af64aae5a85bbc7c4d47b21ed2150
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-05-13 20:01:57 -0400

    creating models

--- techsort/models.py
+++ techsort/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class PrimaryTechnologyAreas(models.Model):
+    TA_1 = 'Launch Propulsion Systems'
+    TA_2 = 'In-Space Propulsion Technologies'
+    TA_3 = 'Space Power and Energy Storage'
+    TA_4 = 'Robotics and Autonomous Systems'
+    TA_5 = 'Communications, Navigation, and Orbital Debris Tracking and Characterization Systems'
+    TA_6 = 'Human Health, Life Support, and Habitation Systems'
+    TA_7 = 'Human Exploration Destination Systems'
+    TA_8 = 'Science Instruments, Observatories, and Sensor Systems'
+    TA_9 = 'Entry, Descent, and Landing Systems'
+    TA_10 = 'Nanotechnology'
+    TA_11 = 'Modeling, Simulation, Information Technology and Processing'
+    TA_12 = 'Materials, Structures, Mechanical Systems and Manufacturing'
+    TA_13 = 'Ground and Launch Systems'
+    TA_14 = 'Thermal Management Systems'
+    TA_15 = 'Aeronautics'
