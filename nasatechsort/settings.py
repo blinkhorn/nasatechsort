131601ccddbf883565d21e7befaf89b4dce47f88
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-06-17 11:48:19 -0400

    changes to project

--- nasatechsort/settings.py
+++ nasatechsort/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'techsort.apps.TechsortConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
